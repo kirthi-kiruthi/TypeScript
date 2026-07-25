a291ec664dce35d2f21c7dd58059aed0ec7c827b
kirthi-kiruthi <kirthimurugan27@gmail.com>
2026-07-25 08:23:40 +0000

    datatypes

---
+++ Day1-TypeScript Intro/index.js
@@ -0,0 +1,37 @@
+//Let 
+let a = 10;
+console.log(a);
+let ArrofData = ["Kiruthika", "Annaya", "Murugan"]; //String
+console.log(ArrofData);
+let ArrOfUsers = ["0.00", "0.99", "0.88"]; //
+console.log(ArrOfUsers);
+let Arrofclass = [12, 23, 24];
+console.log(Arrofclass);
+let ArrofJob = ['True', 'False'];
+console.log(ArrofJob);
+//Const --> Any value assigned to const will be the type of the constant.
+// Value itself become the type.
+const Gender1 = "Male";
+console.log(Gender1);
+const gender = "Female";
+console.log(gender);
+const pincode = 605602;
+console.log(pincode);
+const injob = true;
+console.log(injob);
+//Type Annotations:
+let student;
+student = {
+    name: "Ananya",
+    studentID: 973,
+    course: "Typescript",
+};
+console.log(student);
+let arrofrelatives;
+//arrofrelatives = ["kirthi", "ananya", "nisha"];
+arrofrelatives = ["null", "undefined", 'true'];
+console.log(arrofrelatives);
+let arrof2user;
+arrof2user = [{ name: "kirthi ", age: 34, course: "typescipt" }];
+console.log(arrof2user);
+export {};
---
+++ Day1-TypeScript Intro/index.ts
@@ -0,0 +1,71 @@
+//Let 
+
+let a = 10
+console.log(a)
+
+let ArrofData =["Kiruthika","Annaya", "Murugan"]; //String
+console.log(ArrofData);
+
+let ArrOfUsers= ["0.00","0.99","0.88"]; //
+console.log(ArrOfUsers);
+
+let Arrofclass = [12,23,24];
+console.log(Arrofclass);
+
+let ArrofJob = ['True', 'False'];
+console.log(ArrofJob);
+export {};
+
+//Const --> Any value assigned to const will be the type of the constant.
+// Value itself become the type.
+
+const Gender1= "Male";
+console.log(Gender1);
+
+const gender: string = "Female";
+console.log(gender);
+
+const pincode: Number = 605602;
+console.log(pincode);
+
+const injob: boolean = true;
+console.log(injob);
+
+//Type Annotations:
+
+let student : {
+    name : string;
+    studentID : number;
+    course : string | number;
+}
+
+student = {
+    name : "Ananya",
+    studentID : 973,
+    course : "Typescript",
+}
+console.log(student);
+
+export {};
+
+let arrofrelatives : (String | null| undefined| boolean)[];
+//arrofrelatives = ["kirthi", "ananya", "nisha"];
+arrofrelatives = ["null", "undefined",'true'];
+console.log(arrofrelatives);
+
+let arrof2user : {
+    name : string;
+    age : number;
+    course : string | number}[];
+
+arrof2user = [{name: "kirthi ", age:34, course :"typescipt"}];
+console.log(arrof2user);
+
+
+
+
+
+
+
+
+
