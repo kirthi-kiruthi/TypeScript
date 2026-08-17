d89d396fc6fe50081c269ebc9548d005529d10ce
kirthi-kiruthi <kirthimurugan27@gmail.com>
2026-08-17 08:23:10 +0000

    Functions

--- Day10- Functions/Functions.js
+++ Day10- Functions/Functions.js
@@ -1,33 +1,25 @@
 "use strict";
-function projecttype() {
-    let testedby = "SQA";
-    let duration = "45days";
-    let deadline = "testedby" + "duration";
-    return deadline;
-}
-let res = projecttype();
-console.log(res);
-// function projecttype () {
-//     let duration = "45days"
-//     let deadline = "testedBy" - "Duration"
-//     return deadline
-// }
-// let res = projecttype() 
-// console.log(res)  // No result it will throw errror.
-//Functions:
-//1.Named function
-//2.Ananoyumus fuction
-//3.Function Expression
-//4.IIFE function
-//5.Arrow function
-//6.Higher Order function
-//7.Callback Function
-function calfruits() {
-    let a = "Apple";
-    let b = "Orange";
-    let c = ["Strawberry", "Plum", "Rambutan"];
-    let d = a + " " + b + "," + c;
-    return d;
-}
-let res2 = calfruits();
-console.log(res2);
+//Function all these are named function
+let sumOfNums = function (a, b, c) {
+    return a + b + c;
+};
+let data = sumOfNums(23, 44, "Kiruthika");
+console.log(data);
+//Function Expression:
+let mycompany = function (a, b) {
+    console.log("This is function expression");
+    return a + b;
+};
+let data1 = mycompany(23, 1233445);
+console.log(data1);
+let qspiders = function (a, b) {
+    return a + b;
+};
+let data2 = qspiders("Amarjeet", "Kumar");
+console.log(data2);
+//IIFE Function
+//Syntax : (declaration)(exceution)
+(function (a, b) {
+    console.log("This is Typescript class");
+    console.log(a + b);
+})(8.655656456, "kiruthika");
--- Day10- Functions/Functions.ts
+++ Day10- Functions/Functions.ts
@@ -1,39 +1,96 @@
 //Function all these are named function
 
-function projecttype () {
-    let testedby = "SQA";
-    let duration = "45days";
-    let deadline = "testedby" + "duration";
-    return deadline;
-}
-let res = projecttype();
-console.log(res);
-
 // function projecttype () {
-//     let duration = "45days"
-//     let deadline = "testedBy" - "Duration"
-//     return deadline
+//     let testedby = "SQA";
+//     let duration = "45days";
+//     let deadline = "testedby" + "duration";
+//     return deadline;
 // }
-// let res = projecttype() 
-// console.log(res)  // No result it will throw errror.
+// let res = projecttype();
+// console.log(res);
 
-//Functions:
-//1.Named function
-//2.Ananoyumus fuction
-//3.Function Expression
-//4.IIFE function
-//5.Arrow function
-//6.Higher Order function
-//7.Callback Function
+// // function projecttype () {
+// //     let duration = "45days"
+// //     let deadline = "testedBy" - "Duration"
+// //     return deadline
+// // }
+// // let res = projecttype() 
+// // console.log(res)  // No result it will throw errror.
 
-function calfruits (): string {
-    let a = "Apple";
-    let b = "Orange";
-    let c = ["Strawberry", "Plum", "Rambutan"];
-    let d = a + " "+ b + ","+ c;
-    return d;
-}
-let res2 = calfruits();
-console.log(res2);
+// //Functions:
+// //1.Named function
+// //2.Ananoyumus fuction
+// //3.Function Expression
+// //4.IIFE function
+// //5.Arrow function
+// //6.Higher Order function
+// //7.Callback Function
+
+// function calfruits (): string {
+//     let a = "Apple";
+//     let b = "Orange";
+//     let c = ["Strawberry", "Plum", "Rambutan"];
+//     let d = a + " "+ b + ","+ c;
+//     return d;
+// }
+// let res2 = calfruits();
+// console.log(res2);
 
 //we can define function in type (=>)
+
+//Type Function
+
+// type Function = {
+//     (a: string , b: string , c: number): string | number ;
+// };
+
+// let Sumofstrings: Function = function(a, b, c){
+//      return a*b*c;
+// };
+
+// let data = Sumofstrings("Kiruthika", "Murugan" , 21)
+// console.log (data);
+
+type FunctionType = {
+    (a: number,b: number,c: string): number |string
+}
+
+let sumOfNums: FunctionType  = function(a, b, c) {
+    return a + b + c
+}
+
+let data = sumOfNums(23, 44, "Kiruthika")
+console.log(data)
+
+
+//Function Expression:
+
+let mycompany = function (a : number , b : number)
+{ 
+    console.log ("This is function expression");
+    return a + b;
+}
+let data1 = mycompany(23, 1233445);
+console.log(data1);
+
+
+
+//Sample program 
+
+type trainer = { 
+   ( a : string , b : string) : string;
+}
+let qspiders : trainer = function (a,b)
+{
+    return a + b;
+};
+let data2 = qspiders ("Amarjeet" , "Kumar");
+console.log(data2);
+
+//IIFE Function
+
+//Syntax : (declaration)(exceution)
+(function(a : number , b : string){
+    console.log("This is Typescript class");
+    console.log(a + b);
+})(8.655656456 , "kiruthika");
