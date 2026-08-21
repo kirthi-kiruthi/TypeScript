e5389f8ff92dfa4774d377cb6d5ff030272e4316
kirthi-kiruthi <kirthimurugan27@gmail.com>
2026-08-21 08:19:15 +0000

    String Methods

---
+++ Da12-StringandMethods/String.js
@@ -0,0 +1,24 @@
+"use strict";
+let string1 = "Kiruthika";
+let string2 = 'Nishanthi';
+let string3 = `Murugan`;
+let string4 = new String(true);
+console.log(string1);
+console.log(string2);
+console.log(string3);
+console.log(string4);
+//at and charAt
+let var1 = "Sharanya";
+let res = var1.at(1);
+console.log(res);
+let var2 = "Saranya";
+let res1 = var2.charAt(2);
+console.log(res1);
+//CharCodeAt
+let var3 = "Sivanarul";
+let res2 = var3.charCodeAt(2);
+console.log(res2);
+//Concat
+let var4 = "Kiruthika";
+let res5 = var2.concat(var4);
+console.log(res5);
